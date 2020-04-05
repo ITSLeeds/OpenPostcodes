a86cafaa5d691984df34be3e3627c4a6eb03c58e
Malcolm Morgan <mem48@users.noreply.github.com>
2020-04-05 12:55:22 +0100

    need a bigger computer

---
+++ R/functions.R
@@ -0,0 +1,125 @@
+process_postcodes <- function(oa_sub, postcodes_sub, other_sub){
+  #qtm(oa_sub, fill = NULL) + qtm(postcodes_sub, dots.col = "blue") + qtm(other_sub, dots.col = "red")
+  
+  postcodes_sub <- sf::st_as_sf(postcodes_sub, coords = c("X","Y"), crs = 27700)
+  
+  if(nrow(other_sub) > 0){
+    other_sub <- sf::st_as_sf(other_sub, coords = c("X","Y"), crs = 27700)
+    other_sub <- other_sub[oa_sub,]
+  }
+  
+  if(nrow(postcodes_sub) == 0){
+    message(paste0("No postocdes in OA: ", oa_sub$label))
+    return(NULL)
+  }else{
+    if(nrow(other_sub) > 0){
+      other_sub$quality <- NA
+      postcodes_plus <- rbind(postcodes_sub, other_sub)
+      postcodes_mp <- sf::st_combine(postcodes_plus)
+      
+    } else {
+      postcodes_mp <- sf::st_combine(postcodes_sub)
+    }
+    
+    voronoi <- try(sf::st_voronoi(postcodes_mp, envelope = oa_sub$geometry), silent = TRUE)
+    if("try-error" %in% class(voronoi)){
+      ennv <- sf::st_buffer(oa_sub, 1000)
+      voronoi <- sf::st_voronoi(postcodes_mp, envelope = ennv$geometry)
+    }
+    voronoi <- sf::st_collection_extract(voronoi)
+    sf::st_crs(voronoi) <- 27700
+    voronoi <- sf::st_as_sf(data.frame(id = 1:length(voronoi), geometry = voronoi))
+    suppressWarnings(voronoi <- sf::st_intersection(voronoi, oa_sub))
+    if(nrow(other_sub) > 0){
+      voronoi <- sf::st_join(voronoi, postcodes_plus)
+    } else {
+      voronoi <- sf::st_join(voronoi, postcodes_sub)
+    }
+    
+    names(voronoi) <- c("id","oa","postcode","qua","geometry")
+    voronoi <- voronoi[,c("postcode","oa")]
+    
+    voronoi <- dplyr::group_by(voronoi, postcode)
+    voronoi <- dplyr::summarise(voronoi, oa = oa[1])
+    
+    #qtm(oa_sub, fill = NULL) + qtm(postcodes_sub, dots.col = "blue") + qtm(other_sub, dots.col = "red") + qtm(voronoi, fill = NULL)
+    #qtm(voronoi, fill = "postcode")
+    
+    return(voronoi)
+  }
+  
+}
+
+
+make_postcodes <- function(i){
+  oa_sub <- oa[i,]
+  postcodes_sub <- inter[i][[1]]
+  postcodes_sub <- postcodes[postcodes_sub, ]
+  
+  other_sub <- other[other$postcode %in% postcodes_sub$postcode,]
+  if(nrow(other_sub) > 0){
+    other_sub <- other_sub[oa_sub,]
+  }
+  
+  
+  #qtm(oa_sub, fill = NULL) + qtm(postcodes_sub, dots.col = "blue") + qtm(other_sub, dots.col = "red")
+  
+  if(nrow(postcodes_sub) == 0){
+    message(paste0("No postocdes in OA: ", oa_sub$label))
+    return(NULL)
+  }else{
+    if(nrow(other_sub) > 0){
+      other_sub$quality <- NA
+      postcodes_plus <- rbind(postcodes_sub, other_sub)
+      postcodes_mp <- sf::st_combine(postcodes_plus)
+      
+    } else {
+      postcodes_mp <- sf::st_combine(postcodes_sub)
+    }
+    
+    voronoi <- try(sf::st_voronoi(postcodes_mp, envelope = oa_sub$geometry), silent = TRUE)
+    if("try-error" %in% class(voronoi)){
+      ennv <- sf::st_buffer(oa_sub, 1000)
+      voronoi <- sf::st_voronoi(postcodes_mp, envelope = ennv$geometry)
+    }
+    voronoi <- sf::st_collection_extract(voronoi)
+    sf::st_crs(voronoi) <- 27700
+    voronoi <- sf::st_as_sf(data.frame(id = 1:length(voronoi), geometry = voronoi))
+    suppressWarnings(voronoi <- sf::st_intersection(voronoi, oa_sub))
+    if(nrow(other_sub) > 0){
+      voronoi <- sf::st_join(voronoi, postcodes_plus)
+    } else {
+      voronoi <- sf::st_join(voronoi, postcodes_sub)
+    }
+    
+    names(voronoi) <- c("id","oa","postcode","qua","geometry")
+    voronoi <- voronoi[,c("postcode","oa")]
+    
+    voronoi <- dplyr::group_by(voronoi, postcode)
+    voronoi <- dplyr::summarise(voronoi, oa = oa[1])
+    
+    #qtm(oa_sub, fill = NULL) + qtm(postcodes_sub, dots.col = "blue") + qtm(other_sub, dots.col = "red") + qtm(voronoi, fill = NULL)
+    #qtm(voronoi, fill = "postcode")
+    
+    return(voronoi)
+  }
+  
+}
+
+
+build_list <- function(i){
+  oa_sub <- oa[i,]
+  postcodes_sub <- inter[i][[1]]
+  postcodes_sub <- postcodes[postcodes_sub, ]
+  
+  other_sub <- other[other$postcode %in% postcodes_sub$postcode,]
+  res <- list(oa_sub = oa_sub, postcodes_sub = postcodes_sub, other_sub = other_sub)
+  return(res)
+}
+
+
+make_postcodes2 <- function(sub){
+  res <- process_postcodes(oa_sub = sub$oa_sub, postcodes_sub = sub$postcodes_sub, other_sub = sub$other_sub)
+  return(res)
+}
+
--- R/make_postcodes.R
+++ R/make_postcodes.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(sf)
 library(tmap)
 tmap_mode("view")
+source("R/functions.R")
 
 # Load data
 oa <- readRDS("data-output/England_oa_2001.Rds")
@@ -21,6 +22,7 @@ rm(npe, osm)
 postcodes$postcode <- sapply(postcodes$postcode, function(pc){
   pc_in <- substr(pc, nchar(pc) -2 , nchar(pc))
   pc_out <- substr(pc, 1 , nchar(pc) - 3)
+  pc_out <- gsub(" ","",pc_out)
   pc_fin <- paste0(pc_out," ",pc_in)
   return(pc_fin)
 })
@@ -28,73 +30,46 @@ postcodes$postcode <- sapply(postcodes$postcode, function(pc){
 # Make intersect
 inter <- st_intersects(oa, postcodes)
 
-make_postcodes <- function(i){
-  oa_sub <- oa[i,]
-  postcodes_sub <- inter[i][[1]]
-  postcodes_sub <- postcodes[postcodes_sub, ]
-  
-  other_sub <- other[other$postcode %in% postcodes_sub$postcode,]
-  if(nrow(other_sub) > 0){
-    other_sub <- other_sub[oa_sub,]
-  }
-  
-  
-  #qtm(oa_sub, fill = NULL) + qtm(postcodes_sub, dots.col = "blue") + qtm(other_sub, dots.col = "red")
-  
-  if(nrow(postcodes_sub) == 0){
-    message(paste0("No postocdes in OA: ", oa_sub$label))
-    return(NULL)
-  }else{
-    if(nrow(other_sub) > 0){
-      other_sub$quality <- NA
-      postcodes_plus <- rbind(postcodes_sub, other_sub)
-      postcodes_mp <- sf::st_combine(postcodes_plus)
-      
-    } else {
-      postcodes_mp <- sf::st_combine(postcodes_sub)
-    }
-    
-    voronoi <- try(sf::st_voronoi(postcodes_mp, envelope = oa_sub$geometry), silent = TRUE)
-    if("try-error" %in% class(voronoi)){
-      ennv <- sf::st_buffer(oa_sub, 1000)
-      voronoi <- sf::st_voronoi(postcodes_mp, envelope = ennv$geometry)
-    }
-    voronoi <- sf::st_collection_extract(voronoi)
-    st_crs(voronoi) <- 27700
-    voronoi <- sf::st_as_sf(data.frame(id = 1:length(voronoi), geometry = voronoi))
-    suppressWarnings(voronoi <- st_intersection(voronoi, oa_sub))
-    if(nrow(other_sub) > 0){
-      voronoi <- sf::st_join(voronoi, postcodes_plus)
-    } else {
-      voronoi <- sf::st_join(voronoi, postcodes_sub)
-    }
-    
-    names(voronoi) <- c("id","oa","postcode","qua","geometry")
-    voronoi <- voronoi[,c("postcode","oa")]
-    
-    voronoi <- dplyr::group_by(voronoi, postcode)
-    voronoi <- dplyr::summarise(voronoi, oa = oa[1])
-      
-    #qtm(oa_sub, fill = NULL) + qtm(postcodes_sub, dots.col = "blue") + qtm(other_sub, dots.col = "red") + qtm(voronoi, fill = NULL)
-    #qtm(voronoi, fill = "postcode")
-    
-    return(voronoi)
-  }
-  
-}
+# Compress datasets
+postcodes = cbind(postcodes, st_coordinates(postcodes))
+postcodes = st_drop_geometry(postcodes)
 
+other = cbind(other, st_coordinates(other))
+other = st_drop_geometry(other)
+
+
+# build a list of datasets
 gc()
-pb <- utils::txtProgressBar(max = nrow(oa), style = 3)
-progress <- function(n) utils::setTxtProgressBar(pb, n)
-opts <- list(progress = progress)
-cl <- parallel::makeCluster(4)
-doSNOW::registerDoSNOW(cl)
-boot <- foreach::foreach(i = 1:nrow(oa), .options.snow = opts)
-postcodes_poly <- foreach::`%dopar%`(boot, make_postcodes(i))
-parallel::stopCluster(cl)
-rm(cl, boot, opts, pb, progress)
+masterlist <- pbapply::pblapply(1:nrow(oa), build_list)
+rm(inter, oa, other, postcodes)
+gc()
 
-#postcodes_poly <- pbapply::pblapply(1:nrow(oa), make_postcodes)
+postcodes_poly <- pbapply::pblapply(masterlist, make_postcodes2)
+
+
+
+
+# pb <- utils::txtProgressBar(max = nrow(oa), style = 3)
+# progress <- function(n) utils::setTxtProgressBar(pb, n)
+# opts <- list(progress = progress)
+# cl <- parallel::makeCluster(4)
+# doSNOW::registerDoSNOW(cl)
+# boot <- foreach::foreach(i = 1:10, .options.snow = opts)
+# postcodes_poly <- foreach::`%dopar%`(boot, make_postcodes(i))
+# parallel::stopCluster(cl)
+# rm(cl, boot, opts, pb, progress)
+cl <- parallel::makeCluster(3)
+# parallel::clusterExport(
+#   cl = cl,
+#   varlist = c("inter", "oa", "other", "postcodes"),
+#   envir = environment()
+# )
+parallel::clusterExport(cl, "process_postcodes")
+postcodes_poly <- pbapply::pblapply(masterlist, make_postcodes2, cl = cl)
+parallel::stopCluster(cl)
+rm(cl)
+
+postcodes_poly <- pbapply::pblapply(1:nrow(oa), make_postcodes)
 postcodes_fin <- dplyr::bind_rows(postcodes_poly)
 postcodes_fin <- as.data.frame(postcodes_fin)
 postcodes_fin <- st_as_sf(postcodes_fin)
